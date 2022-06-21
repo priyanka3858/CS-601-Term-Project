a9823c13153333a8e564d4dc44609620233b7674
priyanka <priyanka3858@gmail.com>
2022-06-21 13:12:12 -0400

    chaneg travel and contact page

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,62 +1,205 @@
+<!-- <script>
+export default {
+  name: "aa",
+  data() {
+    return {
+      image: "../assets/background_image.jpeg",
+    };
+  },
+};
+</script> -->
+
 <template>
   <body>
-    <section id="intro-section" class="section-info">
-      <div class="container1">
-        <div id="name">
-          <header>
-            <h1>Priyanka Vekariya</h1>
-            <p class="degree">Software Develpoer</p>
-            <button class="button">Hire me</button>
-          </header>
-        </div>
-        <!-- End Intro section -->
-        <br />
-
-        <!-- Start About section -->
-        <div>
-          <img
-            class="bio-image"
-            src="../assets/coder.png"
-            alt="myimage"
-            width="380"
-            height="250"
-          />
-        </div>
-      </div>
-
-      <h2><b>About Me</b></h2>
-
-      <div class="about">
-        <ul>
-          <li>Name: Priyanka Vekariya</li>
-          <li>Hobby : Yoga,Running,Cooking</li>
-          <li>College: Boston University</li>
-        </ul>
+    <main id="intro-section" class="section-info">
+      <div class="my-intro">
+        <h1><b>MY UNEXPECTED PATH </b></h1>
       </div>
 
       <div id="intro-para" class="my-info">
-        <p class="paragraph">
-          My name is Priyanka vekariya. I graduated in Accounting and Finance
-          from India. After completing my Bechaloree degree, I did three years
-          of internship in a chartered accountant firm. My expertise in audit,
-          taxation, and accounting. But, after working for years, I realized
-          that being an Accountant is not a skill but, rather, practice. For the
-          last two years, I've been heavily involved in Computer Science courses
-          by acquiring knowledge of HTML, CSS, and JavaScript. But I learn that
-          Computer Science is not just learning some programing language; it's
-          much more than that. That's why I decided to go back to school and
-          acquire the necessary skill set to become successful in my career.
+        <p class="paragraph-intro">
+          Priyanka Vekariya is my name. My life path has been anything but
+          predictable. In India, I earned a bachelor's degree in accounting and
+          finance. After finishing my Bachelor's degree, I interned for three
+          years with a chartered accounting business. My auditing, taxation, and
+          accounting knowledge. However, after years of working there, I rapidly
+          recognized that it was not my type of area or lifestyle. I believe
+          that one's work should be meaningful or have an impact on one's life.
+          I've been highly immersed in Computer Science courses for the previous
+          two years, learning HTML, CSS, and JavaScript. But I'm finding that
+          Computer Science is much more than just learning a programming
+          language. That is why I chose to return to school and obtain a
+          degree.That's why I decided to go back to school and acquire the
+          necessary skill set to become successful in my career.
         </p>
-        <p class="paragraph">
-          I have applied and got admission for a master of science in software
-          development(MSSD) at Boston University. I have completed four Courses
-          in MSSD. Between this, I have learned so much from this Master's
-          program. In my personal life, I am passionate about cooking. I have
-          learned almost all Indian vegetarian dishes. I also admire doing yoga
-          and running in real life.
+        <p class="paragraph-intro">
+          I applied for and was accepted to Boston University's master of
+          science in software development (MSSD) program. I have finished four
+          MSSD courses. Between all of this, I've learned a lot from this
+          Master's degree. Cooking is something I enjoy doing in my spare time.
+          Almost all Indian vegetarian recipes are now second nature to me. In
+          real life, I appreciate people who do yoga and running. I am a web
+          developer that enjoys designing visually appealing websites. I have a
+          business background and a strong desire to learn new technology. 
         </p>
       </div>
       <!-- End About section -->
+
+      <!-- this section goes over some of the major publications where my work has been featured -->
+      <section id="publications" class="single_column_section">
+        <h2>Where My Work Has Been Published</h2>
+        <p>
+          While working as a professional photographer documenting adventurous
+          mountain weddings and elopements, I've been able to have my photos
+          published in various news publications and wedding blogs. These are
+          just a few of the ones who have featured my photography work.
+        </p>
+
+        <!-- table containing a subset of places my photography work has been published -->
+        <table>
+          <!-- head of the table, with relevant categories about the publications -->
+          <thead class="publication_table_head">
+            <tr>
+              <th>Publication Name</th>
+              <th class="pub_date">Publication Year</th>
+              <th class="pub_locale">Publication Location</th>
+            </tr>
+          </thead>
+
+          <!-- the actual publications themselves -->
+          <tbody>
+            <tr class="odd">
+              <td>
+                <a
+                  href="https://www.womenshealthmag.com/"
+                  target="_blank"
+                  rel="nofollow"
+                  >Women's Health</a
+                >
+              </td>
+              <td class="pub_date">2020</td>
+              <td class="pub_locale">New York</td>
+            </tr>
+            <tr>
+              <td>
+                <a href="https://www.brides.com/" target="_blank" rel="nofollow"
+                  >Brides</a
+                >
+              </td>
+              <td class="pub_date">2018</td>
+              <td class="pub_locale">New York</td>
+            </tr>
+            <tr class="odd">
+              <td>
+                <a
+                  href="https://www.rocknrollbride.com/"
+                  target="_blank"
+                  rel="nofollow"
+                  >Rock n Roll Bride</a
+                >
+              </td>
+              <td class="pub_date">2020</td>
+              <td class="pub_locale">United Kingdom</td>
+            </tr>
+            <tr>
+              <td>
+                <a
+                  href="https://greenweddingshoes.com/"
+                  target="_blank"
+                  rel="nofollow"
+                  >Green Wedding Shoes</a
+                >
+              </td>
+              <td class="pub_date">2018 + 2021</td>
+              <td class="pub_locale">California</td>
+            </tr>
+            <tr class="odd">
+              <td>
+                <a
+                  href="https://www.popsugar.com/"
+                  target="_blank"
+                  rel="nofollow"
+                  >PopSugar</a
+                >
+              </td>
+              <td class="pub_date">2020</td>
+              <td class="pub_locale">California</td>
+            </tr>
+            <tr>
+              <td>
+                <a href="https://www.yahoo.com/" target="_blank" rel="nofollow"
+                  >Yahoo!</a
+                >
+              </td>
+              <td class="pub_date">2020</td>
+              <td class="pub_locale">California</td>
+            </tr>
+            <tr class="odd">
+              <td>
+                <a
+                  href="https://www.rockymountainbride.com/"
+                  target="_blank"
+                  rel="nofollow"
+                  >Rocky Mountain Bride</a
+                >
+              </td>
+              <td class="pub_date">2018 + 2019</td>
+              <td class="pub_locale">Colorado</td>
+            </tr>
+            <tr>
+              <td>
+                <a
+                  href="https://wanderingweddings.com/"
+                  target="_blank"
+                  rel="nofollow"
+                  >Wandering Weddings</a
+                >
+              </td>
+              <td class="pub_date">2019 + 2020 + 2021</td>
+              <td class="pub_locale">Washington</td>
+            </tr>
+            <tr class="odd">
+              <td>
+                <a
+                  href="https://www.sistersinlaw.se/"
+                  target="_blank"
+                  rel="nofollow"
+                  >Sisters in Law</a
+                >
+              </td>
+              <td class="pub_date">2021</td>
+              <td class="pub_locale">Sweden</td>
+            </tr>
+          </tbody>
+        </table>
+      </section>
+
+      <div class="row">
+        <div class="col-lg-6">
+          <ul>
+            <li>
+              <i class="icofont-rounded-right"></i>
+              <strong>Birthday:</strong> 12 October 1996
+            </li>
+            <li>
+              <i class="icofont-rounded-right"></i> <strong>Phone:</strong> +1
+              480-401-8112
+            </li>
+          </ul>
+        </div>
+        <div class="col-lg-6">
+          <ul>
+            <li>
+              <i class="icofont-rounded-right"></i>
+              <strong>City:</strong> Tempe, AZ
+            </li>
+            <li>
+              <i class="icofont-rounded-right"></i>
+              <strong>Email:</strong> rajaprerak@gmail.com
+            </li>
+          </ul>
+        </div>
+      </div>
 
       <!--  Start Technical Skill section-->
       <div id="skill-list">
@@ -74,7 +217,38 @@
           <li>Data Structure and Algorithms</li>
         </ul>
       </div>
-    </section>
+    </main>
     <!-- End Technical Skill section-->
   </body>
 </template>
+
+<style scoped>
+h1 {
+  background-image: -moz-linear-gradient(
+    top,
+    rgba(126, 9, 9, 0.5) 0%,
+    rgba(255, 255, 255, 0.5) 100%
+  );
+  color: #5a665f;
+  font-family: Times, serif;
+  font-weight: 700;
+  font-size: 2rem;
+  line-height: 3.5rem;
+  text-transform: uppercase;
+  text-align: center;
+  padding: 1.2rem;
+  display: block;
+  border: 1px solid #5a665f;
+  margin-bottom: 1rem;
+  border-radius: 10px;
+}
+
+.paragraph-intro {
+  font-size: 20px;
+  text-align: center;
+  line-height: 1.4rem;
+  font-weight: 400;
+  margin: 15px 160px 15px 160px;
+  text-align: justify;
+}
+</style>
